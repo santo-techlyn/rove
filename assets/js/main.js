3a760120ee1dcb857cfb9fab15993d0128d7631e
santo mitro <santo.techlyn@gmail.com>
2024-09-07 15:32:23 +0600

    zsdf

--- assets/js/main.js
+++ assets/js/main.js
@@ -265,42 +265,7 @@
         });
 
         //test
-        const degreeToRadian = (angle) => {
-            return angle * (Math.PI / 180);
-        };
-        
-        const radius = 80;
-        const diameter = radius * 2;
-        
-        // Get all elements with the class "circle"
-        const circles = document.querySelectorAll(".circle");
-        
-        circles.forEach(circle => {
-            circle.style.width = `${diameter}px`;
-            circle.style.height = `${diameter}px`;
-        
-            const text = circle.dataset.text;
-            const characters = text.split("");
-        
-            const deltaAngle = 360 / characters.length;
-            const characterOffsetAngle = 8;
-            let currentAngle = -90;
-        
-            characters.forEach((character, index) => {
-                const span = document.createElement("span");
-                span.innerText = character;
-        
-                const xPos = radius * (1 + Math.cos(degreeToRadian(currentAngle)));
-                const yPos = radius * (1 + Math.sin(degreeToRadian(currentAngle)));
-        
-                const transform = `translate(${xPos}px, ${yPos}px)`;
-                const rotate = `rotate(${(index * deltaAngle) + characterOffsetAngle}deg)`;
-                span.style.transform = `${transform} ${rotate}`;
-        
-                currentAngle += deltaAngle;
-                circle.appendChild(span);
-            });
-        });
+       
         
           
 
